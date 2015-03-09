a5e5bb2c132c3d90190536d78f0087e024aa32ff
VirgilSuteu <suteu.virgil@gmail.com>
2015-03-09 16:23:51 +0200

    Revert "Test version. [disregard this]"
    This reverts commit 497196ada5bc1be3662aa8b98046e14f74d06a87.

--- src/main/java/com/dataclasses/MyLoginInfo.java
+++ src/main/java/com/dataclasses/MyLoginInfo.java
@@ -1,8 +1,8 @@
 package com.dataclasses;
 
 public class MyLoginInfo {
-	public static String username="asdasda";
-	public static String password="asdasdas";
+	public static String username="";
+	public static String password="";
 
 
 }
